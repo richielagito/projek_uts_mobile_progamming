1133359bdf469308f898c89c755120e362158b52
Muhammad Farhan <muhammadfarhanixc@gmail.com>
2024-09-28 14:34:29 +0700

    menambahkan firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:projek_uts_mobile_progamming/screens/login_screen.dart';
 import 'package:projek_uts_mobile_progamming/screens/main_screen.dart';
 import 'package:projek_uts_mobile_progamming/screens/post_thread_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+// Import the generated file
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
