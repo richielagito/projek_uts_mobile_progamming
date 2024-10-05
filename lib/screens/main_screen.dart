0c9ffd7844eaf220978cb090c7776cb379c28045
richielagito <richielagito1@gmail.com>
2024-10-05 23:26:13 +0700

    Revert "membuat search saat di klik di alihkan ke profile user yg di cari"
    This reverts commit 6e42a07695503547e55a3360677953c42741c663.

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:projek_uts_mobile_progamming/screens/home_screen.dart';
 import 'package:projek_uts_mobile_progamming/screens/profile_screen.dart';
 import 'package:projek_uts_mobile_progamming/screens/search_screen.dart';
-import 'package:projek_uts_mobile_progamming/screens/searchprofile_screen.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key, Object? userData});
@@ -17,9 +16,6 @@ class MainScreenState extends State<MainScreen> {
     const HomeScreen(),
     const SearchScreen(),
     const ProfileScreen(),
-    const SearchProfileScreen(
-      username: '',
-    ),
   ];
 
   void onItemTapped(int index) {
